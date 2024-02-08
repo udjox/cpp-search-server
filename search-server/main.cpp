f06a515e7fc03e46b988b42697d35dd8aecca5b2
Egor <yaropolovegor@yandex.ru>
2024-02-08 18:10:32 +0300

    The code style is now based on Google C++ Style (extra indentation has been removed, the IDF function has been renamed, now it is called ComputeIDF). Data is passed to the FindAllDocuments method by a constant reference.

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,26 @@
-// Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь:
+}
+        return matched_documents;
+    }
+};
 
-// Закомитьте изменения и отправьте их в свой репозиторий.
+SearchServer CreateSearchServer() {
+    SearchServer search_server;
+    search_server.SetStopWords(ReadLine());
+
+    const int document_count = ReadLineWithNumber();
+    for (int document_id = 0; document_id < document_count; ++document_id) {
+        search_server.AddDocument(document_id, ReadLine());
+    }
+
+    return search_server;
+}
+
+int main() {
+    const SearchServer search_server = CreateSearchServer();
+
+    const string query = ReadLine();
+    for (const auto& [document_id, relevance] : search_server.FindTopDocuments(query)) {
+        cout << "{ document_id = " << document_id << ", "
+             << "relevance = " << relevance << " }" << endl;
+    }
+}
